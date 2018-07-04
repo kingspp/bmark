44e88757c5fba8ccc9446ad838b28d2d12517c6d
Prathyush SP <kingspprathyush@gmail.com>
2018-07-04 11:17:18 +0530

    [FE] - Added writers

---
+++ pmark/writers/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 02/07/18,
+| **@author:** prathyushsp,
+| **@version:** v0.0.1
+|
+| **Description:**
+| 
+|
+| **Sphinx Documentation Status:** --
+|
+..todo::
+"""
